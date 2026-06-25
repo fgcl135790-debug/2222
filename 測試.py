86c6ef291ac70b41c0c1b3b785886c8780e67c87
fgcl135790-debug <fgcl135790@gmail.com>
2026-06-25 21:51:49 +0800

    Update API key variable name in 測試.py

--- 測試.py
+++ 測試.py
@@ -1,7 +1,5 @@
 from fugle_marketdata import RestClient
 
-client = RestClient(api_key="你的KEY")
+client = RestClient(api_key="你的APIKEY")
 
-quote = client.stock.intraday.quote(symbol="2409")
-
-print(quote)
+print(client)
