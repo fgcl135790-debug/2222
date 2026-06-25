14e156cb98ee740f1aa9f89a3de7eb4099733cf5
fgcl135790-debug <fgcl135790@gmail.com>
2026-06-26 00:54:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,7 @@ import streamlit as st
 st.write("APP START")
 
 from simulation_engine import SimulationEngine
-
 st.write("SIM OK")
+
+from market_analyzer import MarketAnalyzer
+st.write("ANALYZER OK")
