d6ef81b98dcafa932d0ad146da313ec8bdb6f103
fgcl135790-debug <fgcl135790@gmail.com>
2026-06-25 23:56:01 +0800

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -3,9 +3,16 @@ from fugle_marketdata import WebSocketClient
 import inspect
 
 ws = WebSocketClient(api_key="TEST")
-
 stock = ws.stock
 
-st.write(type(stock))
+st.write("subscribe")
 
-st.write(dir(stock))
+st.code(str(inspect.signature(stock.subscribe)))
+
+st.write("on")
+
+st.code(str(inspect.signature(stock.on)))
+
+st.write("connect")
+
+st.code(str(inspect.signature(stock.connect)))
