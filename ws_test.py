0a75a7423a1e70daeb4935d0e1aee70d6c9a68ec
fgcl135790-debug <fgcl135790@gmail.com>
2026-06-26 00:23:31 +0800

    Update ws_test.py

--- ws_test.py
+++ ws_test.py
@@ -1,22 +1,43 @@
 import streamlit as st
 from fugle_marketdata import WebSocketClient
+from fugle_marketdata.websocket.client import (
+    MESSAGE_EVENT,
+    AUTHENTICATED_EVENT,
+    ERROR_EVENT
+)
 
 api_key = st.text_input("API KEY", type="password")
 
-if api_key and st.button("Connect"):
+if api_key and st.button("Start"):
 
-    try:
+    ws = WebSocketClient(api_key=api_key)
 
-        ws = WebSocketClient(api_key=api_key)
+    stock = ws.stock
 
-        stock = ws.stock
+    def on_auth(msg):
+        st.write("AUTH")
+        st.json(msg)
 
-        stock.connect()
+        try:
+            stock.subscribe({
+                "channel": "trades",
+                "symbol": "2330"
+            })
+        except Exception as e:
+            st.error(e)
 
-        st.success("Connected")
+    def on_message(msg):
+        st.write("MESSAGE")
+        st.code(msg)
 
-    except Exception as e:
+    def on_error(err):
+        st.write("ERROR")
+        st.code(str(err))
 
-        st.error(type(e).__name__)
+    stock.on(AUTHENTICATED_EVENT, on_auth)
+    stock.on(MESSAGE_EVENT, on_message)
+    stock.on(ERROR_EVENT, on_error)
 
-        st.code(str(e))
+    stock.connect()
+
+    st.success("Connected")
