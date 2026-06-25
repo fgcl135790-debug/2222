f3b6f04e51ab74d51eaebd75405f692446624635
fgcl135790-debug <fgcl135790@gmail.com>
2026-06-26 00:18:29 +0800

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,8 +1,5 @@
 import streamlit as st
-from fugle_marketdata import WebSocketClient
+from fugle_marketdata.websocket.client import WebSocketClient
 import inspect
 
-ws = WebSocketClient(api_key="TEST")
-stock = ws.stock
-
-st.text(inspect.getsource(stock.__class__))
+st.text(inspect.getsource(WebSocketClient))
