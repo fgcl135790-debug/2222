867f6d32dcc7254e4e7d4273cec6295d2c3a0562
fgcl135790-debug <fgcl135790@gmail.com>
2026-06-25 23:56:49 +0800

    Refactor WebSocketClient method signatures display
    Replaced multiple write statements with a single code statement to display the source of the subscribe method.

--- TEST.py
+++ TEST.py
@@ -5,14 +5,4 @@ import inspect
 ws = WebSocketClient(api_key="TEST")
 stock = ws.stock
 
-st.write("subscribe")
-
-st.code(str(inspect.signature(stock.subscribe)))
-
-st.write("on")
-
-st.code(str(inspect.signature(stock.on)))
-
-st.write("connect")
-
-st.code(str(inspect.signature(stock.connect)))
+st.code(inspect.getsource(stock.subscribe))
